1882d87c3e06401568c87e906a4494b2dee81b56
pureparsa <khalesparsa@gmail.com>
2026-05-27 17:57:19 +0330

    creating models and migrations

---
+++ app/Models/Board.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Board extends Model
+{
+    protected $fillable = ['title'];
+
+public function user(): belongsTo
+{
+    return $this->belongsTo(User::class);
+}
+public function boardLists(): HasMany
+{
+    return $this->hasMany(BoardList::class);
+}
+}
---
+++ app/Models/BoardList.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class BoardList extends Model
+{
+    protected $fillable = ['title'];
+
+    public function board(): BelongsTo
+    {
+        return $this->belongsTo(Board::class);
+    }
+    public function cards(): HasMany
+    {
+        return $this->hasMany(Card::class);
+    }
+}
