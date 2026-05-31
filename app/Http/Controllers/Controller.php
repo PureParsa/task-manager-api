c3c70285a0b13760d98cda890726fa3b4a3c6a5d
pureparsa <khalesparsa@gmail.com>
2026-06-01 00:56:46 +0330

    Create BoardList CRUD

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use http\Client\Curl\User;
+
 abstract class Controller
 {
-    //
+    public function authorize(){
+        if(1){
+            return true;
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BoardlistController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BoardController;
@@ -12,6 +13,6 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/logout', [AuthController::class, 'logout']);
     Route::apiResource('boards', BoardController::class);
-   // Route::apiResource('boards.lists', BoardListController::class);
+    Route::apiResource('boards.lists', BoardListController::class);
    // Route::apiResource('boards.lists.cards', CardController::class);
 });
