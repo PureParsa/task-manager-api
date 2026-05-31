9761c65b30d0754441280c9e7ec6247293b80e30
pureparsa <khalesparsa@gmail.com>
2026-06-01 01:04:05 +0330

    fix

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,13 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use http\Client\Curl\User;
-
 abstract class Controller
 {
-    public function authorize(){
-        if(1){
-            return true;
-        }
-    }
+    //
 }
